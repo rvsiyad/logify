c07837b9070105212ba66ef73d94bb0cfff48d78
rvsiyad <roblesiyad123@gmail.com>
2025-04-03 13:57:46 +0100

    Remove test for console.log with content, add clean workspace aftereach block

--- test/commands.test.js
+++ test/commands.test.js
@@ -10,8 +10,8 @@ const vscode = require('vscode')
 describe('Commands', function () {
   let tempWorkspacePath
 
-  beforeEach(async function () {
-    // Create the temporary workspace folder
+  beforeEach(function () {
+    // Set up a clean workspace before each test
     tempWorkspacePath = path.join(__dirname, 'tempWorkspace')
 
     if (!fs.existsSync(tempWorkspacePath)) {
@@ -20,13 +20,21 @@ describe('Commands', function () {
 
     // Constructs a new file path for a file
     const workspaceFolder = vscode.Uri.file(tempWorkspacePath)
-    await vscode.workspace.updateWorkspaceFolders(0, null, { uri: workspaceFolder })
+    return vscode.workspace.updateWorkspaceFolders(0, null, { uri: workspaceFolder })
   })
 
   afterEach(function () {
-    if (!fs.existsSync(tempWorkspacePath)) {
-      fs.rmdir(tempWorkspacePath)
-    }
+    // Clean up the workspace after each test
+    return new Promise((resolve, reject) => {
+      if (fs.existsSync(tempWorkspacePath)) {
+        fs.rmdir(tempWorkspacePath, (err) => {
+          if (err) reject(err)
+          resolve()
+        })
+      } else {
+        resolve()
+      }
+    })
   })
 
   describe('addConsole() command', function () {
@@ -48,34 +56,9 @@ describe('Commands', function () {
 
         await vscode.commands.executeCommand('logify.addConsole')
 
-        const result = editor.document.getText()
+        const result = await editor.document.getText()
 
         assert.strictEqual(result, 'const myVariable = 42\nconsole.dir(myVariable, { depth: null, color: true })')
-      })
-    })
-
-    describe('when a file has content as well as the variable', function () {
-      before(async function () {
-        // Create a new .js file and add content
-        const document = await vscode.workspace.openTextDocument({
-          content: 'const myVariable = 42\n//This is a comment\n//This is another comment',
-          language: 'javascript'
-        })
-
-        await vscode.window.showTextDocument(document)
-      })
-
-      it('Adds a console underneath the highlighted variable with the content directly underneath', async function () {
-        // Select the highlighted variable
-        const editor = vscode.window.activeTextEditor
-        editor.selection = new vscode.Selection(0, 6, 0, 16)
-
-        await vscode.commands.executeCommand('logify.addConsole')
-
-        const result = editor.document.getText()
-        console.log('🚀🚀🚀 ~ result:', result)
-
-        assert.strictEqual(result, 'const myVariable = 42\nconsole.dir(myVariable, { depth: null, color: true })\n//This is a comment\n//This is another comment')
       })
     })
   })
