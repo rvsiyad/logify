3d501d422e9fe57684240f6c4212869892f1ccfc
rvsiyad <roblesiyad123@gmail.com>
2025-05-01 14:29:26 +0100

    Remove starter code from package.json and extensions file

--- src/extension.js
+++ src/extension.js
@@ -5,28 +5,14 @@
 const vscode = require('vscode')
 const { addConsole } = require('./commands')
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
 /**
+ * This method is called when your extension is activated
+ * Your extension is activated the very first time the command is execute
+ *
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "logify" is now active!')
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('logify.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from logify!')
-	})
-
+	// Main command that adds console.dir
 	const addConsoleCommand = vscode.commands.registerCommand('logify.addConsole', function () {
 		addConsole()
 	})
